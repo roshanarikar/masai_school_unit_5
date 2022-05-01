9f217954f0899e7bdca1547080e84999917d3dae
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-05-01 11:10:09 +0530

    styled compo complete

--- styled_components/src/App.jsx
+++ styled_components/src/App.jsx
@@ -1,54 +1,19 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Button, Radio } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
-import '~antd/dist/antd.css';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {Button} from "./components/Button";
 
-class ButtonSize extends React.Component {
-  state = {
-    size: 'large',
-  };
-
-  handleSizeChange = e => {
-    this.setState({ size: e.target.value });
-  };
-
-  render() {
-    const { size } = this.state;
-    return (
-      <>
-        <Radio.Group value={size} onChange={this.handleSizeChange}>
-          <Radio.Button value="large">Large</Radio.Button>
-          <Radio.Button value="default">Default</Radio.Button>
-          <Radio.Button value="small">Small</Radio.Button>
-        </Radio.Group>
-        <br />
-        <br />
-        <Button type="primary" size={size}>
-          Primary
-        </Button>
-        <Button size={size}>Default</Button>
-        <Button type="dashed" size={size}>
-          Dashed
-        </Button>
-        <br />
-        <Button type="link" size={size}>
-          Link
-        </Button>
-        <br />
-        <Button type="primary" icon={<DownloadOutlined />} size={size} />
-        <Button type="primary" shape="circle" icon={<DownloadOutlined />} size={size} />
-        <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size} />
-        <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
-          Download
-        </Button>
-        <Button type="primary" icon={<DownloadOutlined />} size={size}>
-          Download
-        </Button>
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+     <Button text="primary">Primary</Button>
+     <Button text="default">Default Button</Button>
+     <Button text="dashed">Dashed Button</Button>
+     <br />
+     <Button text="text">Text Button</Button>
+     <Button text="link">Link Button</Button>
+    </div>
+  )
 }
 
-export default () => <ButtonSize />;
+export default App
